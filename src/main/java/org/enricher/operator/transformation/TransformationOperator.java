378b0fa19984dcb911afe8af134d3dbfb5bb9b6a
ardan <ardankiewicz@gmail.com>
2024-10-10 14:53:32 +0200

    Add Transformation operator

---
+++ src/main/java/org/enricher/operator/transformation/TransformationOperator.java
@@ -0,0 +1,16 @@
+package org.enricher.operator.transformation;
+
+import org.apache.flink.api.common.functions.MapFunction;
+import org.enricher.model.InputMessage;
+import org.enricher.model.TransformedMessage;
+
+public class TransformationOperator implements MapFunction<InputMessage, TransformedMessage> {
+
+    @Override
+    public TransformedMessage map(InputMessage inputMessage) {
+        return new TransformedMessage(
+                inputMessage.getValue(),
+                "transformed-" + inputMessage.getValue()
+        );
+    }
+}
